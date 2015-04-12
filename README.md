ac28424b035a04f12c275b8746c9c6f13bc06d79
Fernando Contreras <fercreek@gmail.com>
2015-04-11 20:26:17 -0500

    Adding server.py to read the information received by android app

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Reference.
 http://hardwarehacking.mx/2014/01/15/leccion-20-arduino-sensor-infrarrojo-basico/
 
 [esau0p's project]:http://hardwarehacking.mx/author/esau0p/
+[SavinaRoja project]:https://github.com/SavinaRoja/PyUserInput
---
+++ server.py
@@ -0,0 +1,29 @@
+import select, socket
+from pykeyboard import PyKeyboard
+
+port = 12345  # where do you expect to get a msg?
+bufferSize = 1024 # whatever you need
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('<broadcast>', port))
+s.setblocking(0)
+k = PyKeyboard()
+
+while True:
+    result = select.select([s],[],[])
+    msg = result[0][0].recv(bufferSize)
+    print msg
+
+    if msg == 'octava5':
+        k.tap_key(',')
+    if msg == 'octava4':
+        k.tap_key('.')
+    if msg == 'octava3':
+        k.tap_key('/')
+    if msg == 'octava2':
+        k.tap_key('=')
+    if msg == 'octava0':
+        k.tap_key('[')
+    if msg == 'octava-1':
+        k.tap_key(']')
+    if msg == 'octava-2':
+        k.tap_key('\\')
